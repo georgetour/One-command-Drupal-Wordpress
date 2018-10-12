5d19cbd5f16602f7c14d06f633929f5b16a31bb0
george <george@besmartbesimple.com>
2018-10-12 22:04:35 +0300

    Update shell script to work with ports

--- shell.sh
+++ shell.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 #Project folder from directory variable and the url will be my-project.localhost:9100
-directory="my-project"
-user_name="yourname"
+directory="myproject"
+user_name="yourusername"
 project_port=9100
 
 #Create drupal files with composer
 composer create-project georgetour/drupal-composer-docker-workflow $directory --stability dev --no-interaction
 
 #Create sync directories so you don't get error in linux
-mkdir "$directory"/config && mkdir "$directory"/config/sync
+#mkdir "$directory"/config && mkdir "$directory"/config/sync
 
 #Add user to your project folder
 chown $user_name "$directory"/config -R
@@ -17,12 +17,14 @@ chown $user_name "$directory"/config -R
 #Enter created folder
 cd $directory
 
+
 #Change values to env file so they can be used by docker-compose
 sed 's/myproject/'$directory'/' .env.example > .env
-sed 's/9100/'$project_port'/' .env.example > .env
+sed -i 's/9100/'$project_port'/' .env
 
 #Run the host for drupal
 docker-compose up -d
+
 
 
 
